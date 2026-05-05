88e0343ac153585acce32a2a21818e5ff5cd60b1
Santorio “Mr Miles” Miles <237955567+MrMiless44@users.noreply.github.com>
2026-05-05 07:41:28 -0500

    Remove redirect function

--- apps/api/api/redirect.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'node:http';
-
-export default function handler(_req: IncomingMessage, res: ServerResponse) {
-  res.writeHead(308, {
-    Location: 'https://www.infamousfreight.com/',
-  });
-  res.end();
-}
